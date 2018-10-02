3339b353bdff73414784976afb79291d048c2a7d
jjcarlospv <jjcarlospv>
2018-10-01 22:33:56 -0500

    Primera clase:kotlin

--- app/src/main/java/com/example/jc/miprimeraaplicacion/MainActivity.kt
+++ app/src/main/java/com/example/jc/miprimeraaplicacion/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.jc.miprimeraaplicacion
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "",Toast.LENGTH_SHORT).show()
+    }
+
+    fun btnEvent(view: View){
+        Toast.makeText(this, "BTN TEST",Toast.LENGTH_SHORT).show()
     }
 }
