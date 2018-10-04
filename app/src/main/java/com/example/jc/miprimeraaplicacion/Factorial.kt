2676538494b511015e2e4d91e35d9bc1be1bfe09
jjcarlospv <jjcarlospv>
2018-10-03 22:59:23 -0500

    Tercera clase

--- app/src/main/java/com/example/jc/miprimeraaplicacion/Factorial.kt
+++ app/src/main/java/com/example/jc/miprimeraaplicacion/Factorial.kt
@@ -1,16 +1,92 @@
 package com.example.jc.miprimeraaplicacion
 
-fun main(arg : Array<String>){
+fun main(arg: Array<String>) {
 
-    factorial()
+    var hombre: Hombre = Hombre()
+    hombre.type
+
+    println(AlumnoDaoImpl.Jugador.nombre)
 }
 
-fun factorial(){
+abstract class Persona {
 
-    var numero : Int = readLine()!!.toInt()
-    var resultado = 1
-    for(x in 1..numero){
-        resultado*= x
+    var type: Int = 23
+    abstract fun correr()
+    abstract fun salter()
+}
+
+interface AlumnoDao {
+    fun guardarAlumno()
+    fun buscarAlumno()
+    fun eliminarAlumno()
+}
+
+interface Oficina {
+    fun metodo1()
+    fun metodo2()
+}
+
+class Hombre() : Persona(), Oficina {
+    override fun metodo1() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-    println("El factorial es  $resultado")
+
+    override fun metodo2() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun correr() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun salter() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
+
+class AlumnoDaoImpl : AlumnoDao, Oficina {
+    override fun metodo2() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun metodo1() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun guardarAlumno() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun buscarAlumno() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun eliminarAlumno() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class Permiso<T> {
+        var datos: String = "sd"
+        var nombre: T? = null
+    }
+
+    enum class enum {
+        Casa(0),
+        Oficina(3);
+
+        var indice: Int? = null
+        var valor: Int = 9
+
+        constructor(valor: Int) {
+            this.indice = valor
+        }
+    }
+
+    class Jugador{
+        companion object {
+            val nombre :String = "name"
+        }
+    }
+
 }
